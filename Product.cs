8efeb9761f52c6c4011e76d25a55322617147ff9
Bryan-OLima <bryanosoriolima@gmail.com>
2022-05-10 17:03:08 -0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex032
+{    
+    class Product
+    {
+        private double taxa = 5.0;
+
+        private double _saldo;
+        public int Conta { get; private set; } 
+        public string Nome { get; private set; }
+
+        public int AdicionarConta(int conta)
+        {
+            return Conta = conta;
+        }
+
+        public void AdicionarNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void DepositarValor(double valor)
+        {       
+            _saldo += valor;
+        }
+
+        public void SacarValor(double valor)
+        { 
+            _saldo = (_saldo - valor) - taxa;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Conta
+                + ", Títular: "
+                + Nome
+                + ", Saldo: "
+                + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Ex032
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir numero da conta
+            Console.Write("Entre o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o títular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá um depósito inicial?");
+            Console.Write("(S/N): ");
+            string depoRequest = Console.ReadLine().ToLower();
+           
+            Product pessoa = new Product();
+            pessoa.AdicionarConta(conta);
+            pessoa.AdicionarNome(nome);
+
+            string resposta = "Dados da conta atualizados:";
+            double deposito; 
+            if (depoRequest == "s")
+            {
+                Console.Write("Entre com o valor de depósito inicial: "); 
+                deposito = double.Parse(Console.ReadLine());
+                pessoa.DepositarValor(deposito);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(resposta);
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+            Console.WriteLine("Deseja fazer um depósito?");
+            Console.Write("(S/N): ");
+            depoRequest = Console.ReadLine().ToLower() ;
+
+            if (depoRequest == "s")
+            {
+                Console.Write("Entre com o valor do depósito: ");
+                deposito = double.Parse(Console.ReadLine());
+                pessoa.DepositarValor(deposito);
+            }
+            Console.WriteLine();
+            Console.WriteLine(resposta);
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+            Console.WriteLine("Deseja realizar um saque?");
+            Console.Write("(S/N): ");
+            depoRequest = Console.ReadLine().ToLower();
+
+            if (depoRequest == "s")
+            {
+                Console.Write("Entre com o valor do Saque: ");
+                deposito = double.Parse(Console.ReadLine());
+                pessoa.SacarValor(deposito);
+            }
+            Console.WriteLine();
+            Console.WriteLine(resposta);
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+
+        }
+    }
+}
